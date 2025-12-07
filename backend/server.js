0c395c09cbcf9ac44c44918f288317b8bd3b316d
umairafatima29 <umairafatima897@gmail.com>
2025-12-07 23:41:26 +0500

    fix server.js

--- backend/server.js
+++ backend/server.js
@@ -17,20 +17,12 @@ connectCloudinary();
 app.use(express.json());
 
 // CORS configuration
-app.use(
-  cors({
-    origin: [
-      "https://prescripto-doctor-booking-system-v7.vercel.app",
-      "https://prescripto-doctor-booking-system-tq.vercel.app",
-      "http://localhost:5174",
-      "http://localhost:5173",
-    ],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-
+const corsOptions = {
+    origin:["https://prescripto-doctor-booking-system-v7.vercel.app","https://prescripto-doctor-booking-system-tq.vercel.app"], 
+    optionsSuccessStatus: 200,
+    credentials: true, 
+};
+app.use(cors(corsOptions));
 
 // api endpoint
 app.use("/api/admin", adminRouter);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,9 @@ import Footer from './components/Footer'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
 
+
+
+
 const App = () => {
   return (
     <div className='mx-4 sm::-[10%]'>
