81a94f0fdd452b42ed6ab2d9eb538fe62c1b9727
umairafatima29 <umairafatima897@gmail.com>
2025-12-07 16:40:26 +0500

    fix error

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,43 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import connectDB from './config/mongodb.js' 
-import connectCloudinary from './config/cloudinary.js'
-import adminRouter from './routes/adminRoute.js'
-import doctorRouter from './routes/doctorRoute.js'
-import userRouter from './routes/userRoute.js'
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import adminRouter from "./routes/adminRoute.js";
+import doctorRouter from "./routes/doctorRoute.js";
+import userRouter from "./routes/userRoute.js";
 
 // app config
-const app = express()
-const port = process.env.PORT || 4000
-connectDB()
-connectCloudinary()
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
 
 // middlewares
-app.use(express.json())
+app.use(express.json());
 
-// CORS configuration 
-app.use(cors({
+// CORS configuration
+app.use(
+  cors({
     origin: [
-        'https://prescripto-doctor-booking-system-li.vercel.app',
-        'https://prescripto-doctor-booking-system-cv.vercel.app'
-        
+      "https://prescripto-doctor-booking-system-v7.vercel.app/",
+      "https://prescripto-doctor-booking-system-tq.vercel.app/",
+      "http://localhost:5174",
+      " http://localhost:5173",
     ],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}))
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // api endpoint
-app.use('/api/admin', adminRouter)
-app.use('/api/doctor', doctorRouter)
-app.use('/api/user', userRouter)
+app.use("/api/admin", adminRouter);
+app.use("/api/doctor", doctorRouter);
+app.use("/api/user", userRouter);
 
-app.get('/', (req, res) => {
-    res.send('API WORKING')
-})
+app.get("/", (req, res) => {
+  res.send("API WORKING");
+});
 
-app.listen(port, () => console.log("Server Started on PORT", port))
+app.listen(port, () => console.log("Server Started on PORT", port));
